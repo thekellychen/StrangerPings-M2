81791112a08d89a0486a4dc6ecfed82198025bc4
thekellychen <kell31ch@gmail.com>
2017-01-30 23:01:58 -0500

    person3.java fixed

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -44,7 +44,7 @@ public class Person3 {
 	private String calc(String input) {
 	  String str = "";
 	  for (int i = input.length() - 1; i >= 0; i--) {
-	  	str += str.charAt(i);
+	  	str += input.charAt(i);
 	  }
 	  return str;
 	}
