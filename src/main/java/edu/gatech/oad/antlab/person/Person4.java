86cdff59168506c8a884718ca20b1d5375a8e19f
Rishab Kaup <Rishab Kaup>
2017-01-30 22:25:38 -0500

    Changed file structure

---
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -0,0 +1,57 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 4
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person4 {
+  /** Holds the persons real name */
+  private String name;
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+  public Person4(String pname) {
+    name = pname;
+  }
+    /**
+     * This method should return a string
+     * where each character is 1 greater 
+     * than its previous value.  So
+     * given "abc123" it should return
+     * "bcd234".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+      //Person 4 put your implementation here
+
+        char[] characterArray = input.toCharArray();
+        int i = 0;
+        while (i<input.length()) {      
+            characterArray [i]= (char) (characterArray[i] + 1);
+            i++;	
+        }
+        return String.valueOf(characterArray);
+    }
+    
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the 
+     *         object
+     */
+    public String toString(String input) {
+      return name + calc(input);
+    }
+
+}
+
